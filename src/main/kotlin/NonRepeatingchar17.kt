dd2da7dd2cf58fc93d73758ed0fade26f3a3921e
Bhargavi <bhargavisrib@gmail.com>
2026-03-12 22:17:07 -0500

    question 17

---
+++ src/main/kotlin/NonRepeatingchar17.kt
@@ -0,0 +1,20 @@
+fun main() {
+    print("Enter the character: ")
+    val str = readln()
+    val map = mutableMapOf<Char, Int>()
+
+    for (ch in str) {
+        if (map.containsKey(ch)) {
+            map[ch] = map[ch]!! + 1
+        } else {
+            map[ch] = 1
+        }
+
+    }
+    for(ch in str) {
+        if(map[ch]==1) {
+            println("The first non repeating character is $ch")
+            break
+        }
+    }
+}
